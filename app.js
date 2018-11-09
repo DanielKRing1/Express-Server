c1ab1a51686ade1716e59e8245045e2f35d25a13
DanielKRing1 <DanielKRing1@gmail.com>
2018-11-08 19:29:52 -0800

    Init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// heroku login
+// heroku create
+// git push heroku master
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+// app.all('*', (req) => console.log(req))
+
+app.get('/', (req, res) =>  res.send({ Hello: 'World' }))
+
+app.get('/square/:num', (req, res) => res.send(`${req.params.num**2}`))
+
+app.listen(port, () =>  console.log(`Listening on port ${port}`))
